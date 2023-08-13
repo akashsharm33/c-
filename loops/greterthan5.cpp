48690fa4dacfd650e2a16ce14af19497ba473b3a
akash <akash@akashsharma.com>
2023-08-13 20:36:30 +0530

    first commit

---
+++ loops/greterthan5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    if(n>5)
+    {
+        cout<<"greater number";
+    }
+    else{
+        cout<<"no";
+    }
+}
---
+++ loops/operators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if((n%5==0)&&(n%3==0)){
+        cout<<"it is";
+    }
+    else{
+        cout<<"not";
+    }
+}
